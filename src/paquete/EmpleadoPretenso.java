a46947efb91e82f5f94e3836c854318e38a4c92d
Paula <102638370+paulabonifazi@users.noreply.github.com>
2022-05-03 23:11:43 -0300

    Update EmpleadoPretenso.java
    cuando mergee seleccioné la clase incorrecta, ya lo solucioné

--- src/paquete/EmpleadoPretenso.java
+++ src/paquete/EmpleadoPretenso.java
@@ -1,16 +1,37 @@
 package paquete;
 
+import Interfaces.IPersonaFisica;
 import modelo.Ticket;
 import modelo.TicketEmpleadoPretenso;
 
-public class EmpleadoPretenso {
-//la clase combien llamarla EmpleadoPretenso a Empleado
-	TicketEmpleadoPretenso ticket;
+public class EmpleadoPretenso extends Persona implements IPersonaFisica
+{	
+	private String nombre;
+	private String apellido;
+	private int edad;
+	private TicketEmpleadoPretenso ticket;
+	
+	
+	public EmpleadoPretenso(Domicilio domicilio, String telefono, String mail, String nombUsuario, String contrasenia,
+			String nombre, String apellido, int edad, TicketEmpleadoPretenso ticket) {
+		super(domicilio, telefono, mail, nombUsuario, contrasenia);
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.ticket = ticket;
+	}
+
 
 	public TicketEmpleadoPretenso getTicket() {
 		return ticket;
 	}
 
+
+	public void setTicket(TicketEmpleadoPretenso ticket) {
+		this.ticket = ticket;
+	}
+
+	
 	
 	
 	
