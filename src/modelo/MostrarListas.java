79bf777346ea9945a86c790a4d48457b6655c0e8
kevin <kevinzamora239@gmail.com>
2022-05-06 07:21:07 -0300

    masMewtodos

---
+++ src/modelo/MostrarListas.java
@@ -0,0 +1,15 @@
+package modelo;
+
+public class MostrarListas {
+
+	public void mostrarListaEmpleados() {}
+	
+	public void mostrarListaEmpleadosPretensos() {}
+	
+	public void mostrarEmpleados() {}
+	
+	public void mostrarEmpleadosPretensos() {}
+	
+	public void listaContrataciones() {}
+
+}
