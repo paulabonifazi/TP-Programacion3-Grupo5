cf622a79192b2249d8450a8203ed0ba29e561fd7
AlanGutierrez39 <54786407+AlanGutierrez39@users.noreply.github.com>
2022-05-07 21:38:47 -0300

    Interfaces
    No sé si falta algo más de interfaces. No borré comentarios.

--- src/paquete/EmpleadoPretenso.java
+++ src/paquete/EmpleadoPretenso.java
@@ -1,10 +1,14 @@
 package paquete;
 
+import java.util.ArrayList;
+
 import interfaces.IPersonaFisica;
-import modelo.Ticket;
+import interfaces.MuestraListaEmpleadores;
+import modelo.ListAsignacionEmpleadPretenso;
+import modelo.ListAsignacionEmpleador;
 import modelo.TicketEmpleadoPretenso;
 
-public class EmpleadoPretenso extends Persona implements IPersonaFisica
+public class EmpleadoPretenso extends Persona implements IPersonaFisica, MuestraListaEmpleadores
 {	
 	private String nombre;
 	private String apellido;
@@ -17,8 +21,6 @@ public class EmpleadoPretenso extends Persona implements IPersonaFisica
 		super(domicilio, telefono, mail, nombUsuario, contrasenia);
 		this.ticket = ticket;
 	}
-
-	
 
 	public TicketEmpleadoPretenso getTicket() {
 		return ticket;
@@ -48,15 +50,49 @@ public class EmpleadoPretenso extends Persona implements IPersonaFisica
 		// TODO Auto-generated method stub
 		return edad;
 	}
+
+	@Override
+	public void mostrarListaEmpleadores(ArrayList<Empleador> empleador)
+	{
+		for (int i =0; i<empleador.size(); i++) 
+			System.out.println(empleador.get(i).getNombUsuario());		
+	}
+
+	@Override
+	public void mostrarListaAsignacionDelEmpleadoPretenso(ListAsignacionEmpleadPretenso lista)
+	{
+		for (int i=0; i<lista.getListEmpleadores().size(); i++)
+			System.out.println(lista.getListEmpleadores().get(i).getNombUsuario());
+	}
+
+	@Override
+	public void mostrarResultado(ArrayList<ListAsignacionEmpleador> lista)
+	{
+		boolean coincidencia = false;
+		int contEmpleador = 0;
+		ListAsignacionEmpleador empleadorActual = null;
+		while (coincidencia==false && contEmpleador < lista.size()) {
+			int contEmpleadoPretenso = 0;
+			//comienzo la busqueda en el nodo
+			empleadorActual = lista.get(contEmpleador);
+			while (coincidencia==false && contEmpleadoPretenso <= empleadorActual.getListEmpleadosPretensos().size()) {
+				if (empleadorActual.getListEmpleadosPretensos().get(contEmpleadoPretenso).getNombUsuario().equals(this.getNombUsuario()))///ver si no hay otra forma de buscar
+					coincidencia = true;
+				else
+					contEmpleadoPretenso++;
+			}
+			contEmpleador++;
+		}
+		if (coincidencia == false)//si coincidencia es false -> nadie lo contrato -> lanza exception u cartel
+			System.out.println("Nadie contrató a " + this.getNombUsuario());
+		else
+			System.out.println("Hay coincidencia entre " + this.getNombUsuario() + " y " + empleadorActual.getEmpleador().getNombUsuario());
+	}
 	
 	///quien se encarga de hacer el registro?? (punto 1 funcionalidad emplead)
 	//excepcionesdelLogin, hace falta la clase login???
 	////metodo que muestre las listas (agencia tambien necesita este metodo -> ver donde ponerlo para no escribirlo 2 veces)	aunque agencia mostraria todos los nodos y esto el suyo unicamente
 				//metodo que me muestre la lista de empresas (accediendo a la lista de agencia)
 				///metodo que me permita seleccionar los posibles empresas (accediendo a la lista de agencia)
-				///metodo que me muestre la opcione elegidas
-	
-	
-	
+				///metodo que me muestre la opcione elegidas	
 }
-
