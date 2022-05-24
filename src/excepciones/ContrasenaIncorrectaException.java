22bbdf4843d7d3569e17b1f0e65ec304199bf74b
Wendy14789 <104234844+Wendy14789@users.noreply.github.com>
2022-05-24 13:50:47 -0300

    Update ContrasenaIncorrectaException.java

--- src/excepciones/ContrasenaIncorrectaException.java
+++ src/excepciones/ContrasenaIncorrectaException.java
@@ -5,12 +5,12 @@ public class ContrasenaIncorrectaException extends Exception
 	
 	public ContrasenaIncorrectaException(String arg0)
 	{
-		super(arg0);
+		super(arg0); 
 	}
 
 	@Override
 	public String getMessage()
 	{
-		return "Contraseña incorrecta";
+		return "ContraseÃ±a incorrecta";
 	}
 }
