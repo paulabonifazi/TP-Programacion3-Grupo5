07861f9839246a2e44e8e57cb8107dc12adb3b91
kevinZamora <78385154+nivek271@users.noreply.github.com>
2022-05-28 13:40:43 -0300

    Update IMuestraListasEmpleadores.java

--- src/interfaces/IMuestraListasEmpleadores.java
+++ src/interfaces/IMuestraListasEmpleadores.java
@@ -6,8 +6,7 @@ import modelo.ListAsignacionEmpleadPretenso;
 import modelo.ListAsignacionEmpleador;
 import paquete.EmpleadoPretenso;
 
-
-//interfaz que contiene los métodos que muestran las listas que los empleadores tienen!
+//interfaz que contiene los mÃ©todos que muestran las listas que los empleadores tienen!
 public interface IMuestraListasEmpleadores
 {
 	public void mostrarListaEmpleadosPretensos(ArrayList<EmpleadoPretenso> empleadoPretenso);
